85da8fd534d0befb4cd19cd8ebcce0250d6ed4af
D3v <deviljin112@users.noreply.github.com>
2020-11-12 10:32:47 +0000

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+# Create a tuple with number 1 to 5
+num_tuple = 1, 2, 3, 4, 5
+print(num_tuple)
+
+# Create a dictionary instead
+num_dict = {1: "apple", 2: "milk", 3: "eggs", 4: "bread", 5: "bike"}
+
+for k, v in num_dict.items():
+    print(f"{k}: {v}")
